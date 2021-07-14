204b4e06417f636ae513740bfc0f6359ad7ef48d
Emily <emily.roberts@softwire.com>
2021-07-14 17:09:02 +0100

    Added 2 spinners on iOS

--- native/KotlinIOS/KotlinIOS/ViewController.swift
+++ native/KotlinIOS/KotlinIOS/ViewController.swift
@@ -2,25 +2,62 @@ import UIKit
 import SharedCode
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet private var label: UILabel!
     @IBOutlet private var button: UIButton!
-
-    @IBAction func openURL(_ sender: UIButton ) {
-        let URLstring: String = presenter.onButtonTapped(stationStart: "DHM", stationEnd:"KGX")
-        UIApplication.shared.open(URL(string:URLstring)! as URL, options: [:], completionHandler:nil)
-        
-    }
+    @IBOutlet private var startStation: UIPickerView!
+    @IBOutlet private var endStation: UIPickerView!
+    private var station1: String = ""
+    private var station2: String = ""
     private let presenter: ApplicationContractPresenter = ApplicationPresenter()
+    private let stationData: [String] = ["KGX","EUS", "BHM", "DHM", "YRK"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter.onViewTaken(view: self)
+        self.startStation.delegate = self
+        self.startStation.dataSource = self
+        self.endStation.delegate = self
+        self.endStation.dataSource = self
     }
+    
+
+    @IBAction func openURL(_ sender: UIButton ) {
+        let URLstring: String = presenter.onButtonTapped(stationStart: station1, stationEnd:station2)
+        UIApplication.shared.open(URL(string:URLstring)! as URL, options: [:], completionHandler:nil)
+        
+    }
+    
 }
 
 extension ViewController: ApplicationContractView {
     func setLabel(text: String) {
         label.text = text
     }
+}
+
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ picker: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return stationData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if (pickerView==startStation) {
+            station1 = stationData[row]
+        }
+        if (pickerView==endStation) {
+            station2 = stationData[row]
+        }
+    }
+}
+
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in picker: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ picker: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stationData.count
+    }
+
 }
