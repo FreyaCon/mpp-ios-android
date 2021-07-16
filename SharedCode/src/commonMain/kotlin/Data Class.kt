89384319ce0239e3813d2ff4e92c007709907b61
Freya Connor <Freya.Connor@softwire.com>
2021-07-16 12:25:19 +0100

    formatted Json Data

--- SharedCode/src/commonMain/kotlin/Data Class.kt
+++ SharedCode/src/commonMain/kotlin/Data Class.kt
@@ -1,10 +1,14 @@
 package com.jetbrains.handson.mpp.mobile
 import com.soywiz.klock.Date
 import kotlinx.serialization.*
-
 @Serializable
-data class TrainData(val station1: String
-                     , val station2: String
+data class JsonStation(val displayName:String, val crs:String)
+@Serializable
+data class TrainData(val outboundJourneys:List<Train>)
+@Serializable
+class Train(val originStation: JsonStation
+                     , val destinationStation: JsonStation
                      , val departureTime: String
                      , val arrivalTime: String
+                     , val journeyDurationInMinutes: Int
                      , val status: String)
